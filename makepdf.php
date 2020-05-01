4d0c20c2dab7ba790dc70db56d9dc4ff2c08e2b2
Lahulesh <62867823+Lahulesh@users.noreply.github.com>
2020-05-01 20:07:42 +0530

    Add files via upload

--- makepdf.php
+++ makepdf.php
@@ -1,23 +1,39 @@
-<?php
 
+<?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-$first = $_POST['fn'];
-$Last = $_POST['ln'];
-$Middle = $_POST['mn'];
-$Countr = $_POST['c'];
-$Phone = $_POST['p'];
 
-$mpdf  = new \Mpdf\Mpdf();
 
-$data = '';
-$data .= '<h1>Your Details<h1>';
+$id = $_GET["id"];
 
-$data .= '<Strong>FIRST NAME :-</strong> ' . $First .'<br />';
-$data .= '<Strong>FIRST NAME :-</strong> ' . $Last .'<br />';
-$data .= '<Strong>FIRST NAME :-</strong> ' . $Middle .'<br />';
-$data .= '<Strong>FIRST NAME :-</strong> ' . $Countr .'<br />';
-$data .= '<Strong>FIRST NAME :-</strong> ' . $Phone .'<br />';
 
-$mpdf->WriteHTML($data);
-$mpdf->Output('Myfile.pdf','D');
+include 'conn.php';
+$q = " SELECT * FROM crudtable WHERE  id = $id";  
+$query = mysqli_query($connect,$q);
+
+$result=  mysqli_fetch_array($query);
+
+
+
+$mpdf = new \Mpdf\Mpdf();
+$firstname = $result['firstname'];
+$lastname = $result['lastname'];
+$middlename = $result['middlename'];
+$country = $result['country'];
+$phone = $result['phone'];
+
+$mpdf->WriteHTML('<h1>YOUR DATA PDF!</h1>');
+$html =  "<h1>" . htmlspecialchars($firstname)."</h1>";
+ $html1 =  "<h1>" . htmlspecialchars($lastname)."</h1>";
+ $html2 =  "<h1>" . htmlspecialchars($middlename)."</h1>";
+$html3 =  "<h1>" . htmlspecialchars($country)."</h1>";
+ $html4 =  "<h1>" . htmlspecialchars($phone)."</h1>";
+  
+$mpdf->WriteHTML($html);
+$mpdf->WriteHTML($html1);
+$mpdf->WriteHTML($html2);
+$mpdf->WriteHTML($html3);
+$mpdf->WriteHTML($html4);
+$mpdf->Output();
+
+?>
